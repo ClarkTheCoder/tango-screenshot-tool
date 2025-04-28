b0543c053968db9eef166b7618e0be669f7208f8
Carson Clark <carsonclark@Carsons-MacBook-Air.local>
2025-04-27 19:24:42 -0700

    initial commit

---
+++ Tango/TangoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangoApp.swift
+//  Tango
+//
+//  Created by Carson Clark on 2025-04-27.
+//
+
+import SwiftUI
+
+@main
+struct TangoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
